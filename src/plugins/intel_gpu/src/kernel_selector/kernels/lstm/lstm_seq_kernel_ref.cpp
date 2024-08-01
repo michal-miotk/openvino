54ee91296a4a9031730d0278704a3843bcb0e31d
michal-miotk <michal.miotk@intel.com>
2024-08-01 11:01:55 +0000

    only bfyx layout

--- src/plugins/intel_gpu/src/kernel_selector/kernels/lstm/lstm_seq_kernel_ref.cpp
+++ src/plugins/intel_gpu/src/kernel_selector/kernels/lstm/lstm_seq_kernel_ref.cpp
@@ -15,7 +15,7 @@ ParamsKey LSTMSeqKernelRef::GetSupportedKey() const {
     k.EnableOutputDataType(Datatype::F16);
     k.EnableOutputDataType(Datatype::F32);
     k.EnableDifferentTypes();
-    k.EnableAllInputLayout();
+    k.EnableInputLayout(DataLayout::bfyx);
     k.EnableAllOutputLayout();
     k.EnableTensorOffset();
     k.EnableTensorPitches();
