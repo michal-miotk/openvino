758580992e884e83a30f5ab01aace349f6c7b9b6
Michal Miotk <michal.miotk@intel.com>
2025-03-11 11:43:01 +0100

    added to trans pipeline

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -64,6 +64,7 @@
 #include "plugin/transformations/binary_conv_to_conv.hpp"
 #include "plugin/transformations/clamp_fp16_output.hpp"
 #include "plugin/transformations/convert_fc_to_compressed.hpp"
+#include "plugin/transformations/convert_conv_to_compressed.hpp"
 #include "plugin/transformations/convert_matmul_to_fc.hpp"
 #include "plugin/transformations/convert_stridedslices_to_variadicsplit.hpp"
 #include "plugin/transformations/decompose_reduce_scalar_output.hpp"
@@ -1023,6 +1024,7 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         auto pass_config = manager.get_pass_config();
         manager.register_pass<ov::intel_gpu::KVCacheFusion>();
         manager.register_pass<ov::intel_gpu::FullyConnectedConvertFusion>();
+        manager.register_pass<ov::intel_gpu::ConvertConvolutionToConvolutionCompressed>();
         manager.register_pass<ov::intel_gpu::TransposeFusion>(device_info.supports_immad);
 
         if (!device_info.supports_immad) {
